8b396d552ea065d3a0df89c0a66c4c83db99452e
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-02-01 21:31:13 +0000

    Cleaned into functions and added another check

--- Agents/greedyAgent.py
+++ Agents/greedyAgent.py
@@ -38,28 +38,20 @@ class GreedyAgent():
             if '2c' in hand:
                 smallest_card = '2c'
             else:
-                # choose the legal card with the smallest value to play
-
                 hearts_broken = observation['data']['trickSuit']
                 trick_suit = observation['data']['trickSuit']
                 smallest_card = 14
 
                 if trick_suit == "Unset":
                     if hearts_broken:
-                        for card in hand:
-                            if card[0] == 'A' and 14 <= smallest_card:
-                                smallest_card = card
-                            elif card[0] == 'K' and 13 <= smallest_card:
-                                smallest_card = card
-                            elif card[0] == 'Q' and 12 <= smallest_card:
-                                smallest_card = card
-                            elif card[0] == 'J' and 11 <= smallest_card:
-                                smallest_card = card
-                            elif card[0] == 'T' and 10 <= smallest_card:
-                                smallest_card = card
-                            else:
-                                if int(card[0]) < smallest_card:
-                                    smallest_card = card
+                        smallest_card = find_smallest_card(hand)
+                    else:
+                        no_hearts_hand = self.remove_illegal_cards(hand)
+                        smallest_card = find_smallest_card(no_hearts_hand)
+                else:
+                    # first check if trick suit present in hand; if it is then limit cards to that suit
+                    # otherwise let any card be played (except for hearts if hearts not broken)
+
 
             return {
                 "event_name": "PlayTrick_Action",
@@ -68,4 +60,34 @@ class GreedyAgent():
                     'action': {'card': smallest_card}
                 }
             }
-            
+    
+
+    def find_smallest_card(self):
+
+        # choose the legal card with the smallest value to play
+        for card in hand:
+            if card[0] == 'A' and 14 <= smallest_card:
+                smallest_card = card
+            elif card[0] == 'K' and 13 <= smallest_card:
+                smallest_card = card
+            elif card[0] == 'Q' and 12 <= smallest_card:
+                smallest_card = card
+            elif card[0] == 'J' and 11 <= smallest_card:
+                smallest_card = card
+            elif card[0] == 'T' and 10 <= smallest_card:
+                smallest_card = card
+            else:
+                if int(card[0]) < smallest_card:
+                    smallest_card = card
+        
+        return smallest_card
+
+
+    def remove_illegal_cards(self, hand):
+
+        legal_hand = hand.copy()
+        for card in legal_hand:
+            if card[1] == 'h':
+                del legal[card]
+
+        return legal
